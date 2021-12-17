743eac3fe4b710c640f76d84cc898883a627001c
Daniel Chen <dchen0058@gmail.com>
2021-12-17 21:09:51 +1100

    completed 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs)=>{
   return 1
 }
@@ -18,8 +20,33 @@ const favoriteBlog = (blogs)=>{
       })
 }
 
+const mostBlogs = (blogs)=>{
+
+  if(blogs.length===0){
+    return null
+  }
+
+  const authorBlogCounts = _.countBy(blogs, (blog)=>{
+    return blog.author
+  })
+
+  const authorMostBlog = Object.entries(authorBlogCounts).reduce((most,author)=>{
+    return most[1] > author[1]
+      ? most
+      : author
+  },0)
+
+  const authorMostBlogObject = {
+    author: authorMostBlog[0],
+    blogs: authorMostBlog[1]
+  }
+
+  return authorMostBlogObject
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
