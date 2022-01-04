97f568feb68288dc1cbd0bf6cb4a4c68f70a6c8a
Daniel Chen <dchen0058@gmail.com>
2022-01-04 18:57:26 +1100

    completed 4.16

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    minLength: 3,
+    unique: true,
+  },
   name: String,
   passwordHash: String,
 })
@@ -15,6 +20,8 @@ userSchema.set('toJSON',{
     delete returnedObject.passwordHash
   }
 })
+
+userSchema.plugin(uniqueValidator)
 
 const User = mongoose.model('User', userSchema)
 
